46f4487987695e027c1f8fb6ef1a9a2bd669d1a8
Hammad Majid <hammadmajid@proton.me>
2024-07-18 07:10:24 +0500

    Create Button.svelte that can render four different types of buttons and handle on:click event
    The four types are
    - primary
    - warning
    - danger
    - disabled

---
+++ src/components/Button.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	let buttonType: 'primary' | 'warning' | 'danger' | 'disabled';
+
+	export let text: string;
+	export { buttonType as type };
+</script>
+
+<button on:click={() => dispatch('click')} class={'btn btn-' + buttonType}>{text}</button>
+
+<style>
+    .btn {
+        padding: 1rem;
+        border-radius: 8px;
+        border: none;
+        font-size: 1rem;
+        font-weight: bold;
+        transition: background-color 250ms;
+    }
+
+    .btn:hover {
+        cursor: pointer;
+    }
+
+    .btn-primary {
+        background-color: var(--color-sky);
+        color: var(--color-subtext2);
+
+    }
+
+    .btn-primary:hover {
+        background-color: var(--color-sapphire);
+    }
+
+		.btn-warning {
+				background-color: var(--color-yellow);
+				color: var(--color-subtext2);
+		}
+		.btn-warning:hover {
+				background-color: var(--color-peach);
+		}
+
+		.btn-danger {
+				color: var(--color-subtext2);
+        background-color: var(--color-maroon);
+
+		}
+
+		.btn-danger:hover {
+        background-color: var(--color-red);
+
+		}
+
+		.btn-disabled {
+				background-color: var(--color-overlay2);
+		}
+		.btn-disabled:hover {
+				cursor: not-allowed;
+		}
+</style>
