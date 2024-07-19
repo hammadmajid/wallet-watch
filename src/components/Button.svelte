3eab5e16dfd820350e692306997471980a69ea98
Hammad Majid <hammadmajid@proton.me>
2024-07-20 03:57:42 +0500

    Remove redundent `text:` property by adding it shared `.btn` class and format code

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -11,52 +11,47 @@
 <button on:click={() => dispatch('click')} class={'btn btn-' + buttonType}>{text}</button>
 
 <style>
-    .btn {
-        padding: 1rem;
-        border-radius: 8px;
-        border: none;
-        font-size: 1rem;
-        font-weight: bold;
-        transition: background-color 250ms;
-    }
+	.btn {
+		padding: 1rem;
+		border-radius: 8px;
+		border: none;
+		font-size: 1rem;
+		font-weight: bold;
+		transition: background-color 250ms;
+		color: var(--color-crust);
+	}
 
-    .btn:hover {
-        cursor: pointer;
-    }
+	.btn:hover {
+		cursor: pointer;
+	}
 
-    .btn-primary {
-        background-color: var(--color-sky);
-        color: var(--color-subtext2);
+	.btn-primary {
+		background-color: var(--color-sky);
+	}
 
-    }
+	.btn-primary:hover {
+		background-color: var(--color-sapphire);
+	}
 
-    .btn-primary:hover {
-        background-color: var(--color-sapphire);
-    }
+	.btn-warning {
+		background-color: var(--color-yellow);
+	}
+	.btn-warning:hover {
+		background-color: var(--color-peach);
+	}
 
-		.btn-warning {
-				background-color: var(--color-yellow);
-				color: var(--color-subtext2);
-		}
-		.btn-warning:hover {
-				background-color: var(--color-peach);
-		}
+	.btn-danger {
+		background-color: var(--color-maroon);
+	}
 
-		.btn-danger {
-				color: var(--color-subtext2);
-        background-color: var(--color-maroon);
+	.btn-danger:hover {
+		background-color: var(--color-red);
+	}
 
-		}
-
-		.btn-danger:hover {
-        background-color: var(--color-red);
-
-		}
-
-		.btn-disabled {
-				background-color: var(--color-overlay2);
-		}
-		.btn-disabled:hover {
-				cursor: not-allowed;
-		}
+	.btn-disabled {
+		background-color: var(--color-overlay2);
+	}
+	.btn-disabled:hover {
+		cursor: not-allowed;
+	}
 </style>
