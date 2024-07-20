bec0b569238cbcd41e696a2054940c59e2fe853b
Hammad Majid <hammadmajid@proton.me>
2024-07-20 12:11:44 +0500

    Create Input.svelte component to handle user input in forms
    Will add support for date and time fields as well in the future

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let name: string;
+	export let type: 'text' | 'password' | 'email';
+	export let placeholder: string;
+	export let required: boolean = true;
+</script>
+
+<label for={name}>
+	<input {type} {name} id={name} {placeholder} {required} />
+</label>
+
+<style>
+	input {
+		font-size: 1.1rem;
+		outline: none;
+		border: none;
+		border-radius: 8px;
+		width: 100%;
+		padding: 1rem;
+		background-color: var(--color-base);
+		color: var(--color-text);
+	}
+
+	input::placeholder {
+		color: var(--color-subtext1);
+	}
+</style>
