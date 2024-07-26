cbc77a66bc5c1ae2faa02fbca7cb1d7a09792bd6
Hammad Majid <hammadmajid@proton.me>
2024-07-26 21:16:38 +0500

    Add action to signout user and redirect if they aren't signed in

---
+++ src/routes/app/profile/+page.server.ts
@@ -0,0 +1,22 @@
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals: { safeGetSession } }) => {
+	const { session } = await safeGetSession();
+
+	if (!session) {
+		redirect(303, '/auth');
+	}
+
+	return { session };
+};
+
+export const actions: Actions = {
+	signout: async ({ locals: { supabase, safeGetSession } }) => {
+		const { session } = await safeGetSession();
+		if (session) {
+			await supabase.auth.signOut();
+			redirect(303, '/');
+		}
+	}
+};
