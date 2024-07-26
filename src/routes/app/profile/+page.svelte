f26f3d75fcb658350c30f8b1266a769addeaa4c4
Hammad Majid <hammadmajid@proton.me>
2024-07-26 21:16:07 +0500

    Create /app/profile page to show user their profile data

---
+++ src/routes/app/profile/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { Session } from '@supabase/supabase-js';
+	import Button from '$lib/components/Button.svelte';
+
+	interface Props {
+		data: { session: Session | null };
+	}
+	let { data }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>Profile | Fintraq</title>
+</svelte:head>
+
+<section>
+	<h1>Your profile</h1>
+	<p>
+		You are signed in as <strong>{data.session?.user.email}</strong>
+	</p>
+
+	<form method="POST" action="?/signout">
+		<Button type="button" style="warning" text="Signout" />
+	</form>
+</section>
+
+<style>
+	section {
+		padding: 1rem;
+		width: 80%;
+		margin: 0 auto;
+	}
+
+	h1 {
+		margin-bottom: 5px;
+	}
+
+	form {
+		margin-top: 2rem;
+	}
+</style>
