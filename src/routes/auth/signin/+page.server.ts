db41c8c41c2c41bc3609a77f95649c5db108e44c
Hammad Majid <hammadmajid@proton.me>
2024-08-03 04:12:07 +0500

    Use default form action to signin the user or redirect if already signed in

---
+++ src/routes/auth/signin/+page.server.ts
@@ -0,0 +1,35 @@
+import { fail, redirect } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
+    const { session } = await safeGetSession();
+
+    if (session) {
+        redirect(303, '/app/profile');
+    }
+
+    return { url: url.origin };
+}
+
+export const actions: Actions = {
+    default: async (event) => {
+        const {
+            request,
+            locals: { supabase },
+        } = event;
+
+        const formData = await request.formData();
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        
+        const { error } = await supabase.auth.signInWithPassword({email, password});
+
+        if (error) {
+            // TODO: handle this error in +page.svelte
+            return fail(error.status ?? 500, { code: error.code, message: error.message });
+        }
+
+        throw redirect(302, "/app/profile");
+
+    },
+} satisfies Actions;
