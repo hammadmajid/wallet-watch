1771731bfd9839738f5c9a5e70f62c05e8fe56f5
Hammad Majid <hammadmajid@proton.me>
2024-08-03 04:08:18 +0500

    Signup the user with default form action or redirect to profile if already signed in

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,46 @@
+import { fail, redirect } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
+    const { session } = await safeGetSession();
+
+    if (session) {
+        redirect(303, '/app/profile');
+    }
+
+    return { url: url.origin };
+}
+
+export const actions: Actions = {
+    default: async (event) => {
+        const {
+            request,
+            locals: { supabase },
+        } = event;
+
+        const formData = await request.formData();
+        const firstName = formData.get('first_name') as string;
+        const lastName = formData.get('last_name') as string;
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        const currency = formData.get('currency') as string;
+
+        const { error } = await supabase.auth.signUp({
+            email, password, options: {
+                data: {
+                    first_name: firstName,
+                    last_name: lastName,
+                    currency
+                }
+            }
+        });
+
+        if (error) {
+            // TODO: handle this error in +page.svelte
+            return fail(error.status ?? 500, { code: error.code, message: error.message });
+        }
+
+        throw redirect(302, "/app/profile");
+
+    },
+} satisfies Actions;
