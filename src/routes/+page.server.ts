0262b1939d1edb39b8ac5e16a97129e5f740888b
Hammad Majid <hammadmajid@proton.me>
2024-08-03 04:05:35 +0500

    Fix broken link

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	throw redirect(301, '/auth/login');
+	throw redirect(301, '/auth/signin');
 };
