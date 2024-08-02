253e2493275ca6ad8061b28263a8d05a6d496489
Hammad Majid <hammadmajid@proton.me>
2024-08-03 04:07:07 +0500

    Add first, last name fields & refactor styling

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -13,15 +13,30 @@
 		<h1 class="text-2xl font-bold">Create new account</h1>
 		<p>Enter your information below to create new account on Fintraq</p>
 	</div>
-	<form class="space-y-2 w-1/2" method="POST">
+	<form class="space-y-2 lg:w-1/3" method="POST">
+		<div class="flex flex-col gap-2 md:flex-row">
+			<Input type="text" name="first_name" placeholder="First name" />
+			<Input type="text" name="last_name" placeholder="Last name" />
+		</div>
+
 		<Input type="email" name="email" placeholder="Email" />
 		<Input type="password" name="password" placeholder="Password" />
-		<select class="block px-4 py-2 appearance-none">
-			{#each currencies as currency}
-				<option value={currency.code}>{currency.name} - {currency.code}</option>
-			{/each}
-		</select>
+
+		<div>
+			<label for="currency"></label>
+			<select id="currency" name="currency" class="block px-4 py-2 appearance-none">
+				{#each currencies as currency}
+					<option value={currency.code}>{currency.name} - {currency.code}</option>
+				{/each}
+			</select>
+		</div>
 		<Button type="submit">Sign up</Button>
 	</form>
-	<p>Already have an account? <a href="/auth/signin"  class="font-medium text-blue-600 transition-colors duration-300 hover:text-blue-800">Sign in</a></p>
+	<p>
+		Already have an account? <a
+			href="/auth/signin"
+			class="font-medium text-blue-600 transition-colors duration-300 hover:text-blue-800"
+			>Sign in</a
+		>
+	</p>
 </main>
