0f5ef936811551e882c27db38c6ea371d125bff7
Hammad Majid <hammadmajid@proton.me>
2024-08-03 05:13:38 +0500

    Create empty account, dashboard and records page

---
+++ src/routes/app/dashboard/+page.svelte
@@ -0,0 +1,3 @@
+<main class="flex flex-col gap-6 justify-center items-center p-4 mx-auto min-h-screen">
+	<p class="space-x-2"><i class="fa-solid fa-wrench"></i><span>/dashboard under constuction</span></p>
+</main>
