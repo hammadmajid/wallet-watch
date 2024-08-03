f438b23d8e7661d874b112ef2213ebe03b40be4f
Hammad Majid <hammadmajid@proton.me>
2024-08-03 13:24:20 +0500

    Create abstract SelectInput field

---
+++ src/lib/components/SelectInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface Props {
+		name: string;
+		options: string[];
+	}
+
+	let { name, options }: Props = $props();
+</script>
+
+<select
+	{name}
+	class="px-4 py-2 w-full rounded-sm transition-all appearance-none outline-none focus:outline-2 bg-zinc-50 text-zinc-800 focus:outline-slate-800"
+>
+	{#each options as option}
+		<option value={option}>{option}</option>
+	{/each}
+</select>
