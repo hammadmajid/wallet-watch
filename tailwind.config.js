a2aefd4afe11d703f28780f033b5aba32224cd9e
Hammad Majid <hammadmajid@proton.me>
2024-08-07 18:42:39 +0500

    Add skeleton ui library & configure it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+// @ts-check
+import { join } from 'path'
+import { skeleton } from '@skeletonlabs/tw-plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		skeleton({
+			themes: { preset: ['hamlindigo'] }
+		})
+	]
 }
