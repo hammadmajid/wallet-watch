4681dd15782cf1a29719941659d8df39faf6e83e
Hammad Majid <hammadmajid@proton.me>
2024-08-07 23:13:45 +0500

    Add AppBar in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '$lib/app.css'
 	import { invalidate } from '$app/navigation'
 	import { onMount } from 'svelte'
-	import { initializeStores, Modal, Toast } from '@skeletonlabs/skeleton'
+	import { initializeStores, Modal, Toast, AppBar } from '@skeletonlabs/skeleton'
 
 	initializeStores()
 
@@ -24,5 +24,14 @@
 <Modal />
 <Toast />
 <div class="w-screen min-h-screen">
+	<AppBar shadow="drop-shadow-xl" label="header">
+		<svelte:fragment slot="lead"><p class="h3"><a href="/">Fintraq</a></p></svelte:fragment>
+		<svelte:fragment slot="trail"
+			><a href="https://github.com/hammadmajid/fintraq"
+				><i class="w-6 h-6 fa-brands fa-github"></i></a
+			></svelte:fragment
+		>
+	</AppBar>
+
 	<slot />
 </div>
